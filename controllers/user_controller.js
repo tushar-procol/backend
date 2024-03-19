4b1156cece75aaaf77457f29c471a6ee78756ea0
Tushar Garg <tushar.garg@procol.in>
2024-03-19 12:31:31 +0530

    crud on users model

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,17 +1,48 @@
 const db = require("../models");
 
-const addUser = async (req, res) => {
+const add_user = async (req, res) => {
   var User = db.user;
-  // const tushar = User.build({ first_name: "Tushar" });
-  // console.log(jane instanceof User); // true
-  // console.log(jane.name); // "Jane"
-  // Jane exists in the database now!
+  const user_data = req.body;
+  const created_user = await User.create({ ...user_data });
 
-  // await tushar.save();
-  // res.status(200).json(tushar.toJSON());
-
-  const jane = await User.create({ first_name: "Neymar" });
-  res.status(200).json(jane.toJSON());
+  res.status(200).json(created_user.toJSON());
 };
 
-module.exports = { addUser };
+const get_all_users = async (req, res) => {
+  var User = db.user;
+  const users = await User.findAll();
+  res.status(200).json({ data: users });
+};
+
+const get_one_user = async (req, res) => {
+  const User = db.user;
+  const id = req.params.id;
+
+  const user = await User.findAll({ where: { id } });
+  res.status(200).json({ data: user });
+};
+
+const delete_user = async (req, res) => {
+  const User = db.user;
+  const id = req.params.id;
+
+  const user = await User.destroy({ where: { id } });
+  res.status(200).send("successs");
+};
+
+const update_user = async (req, res) => {
+  const User = db.user;
+  const id = req.body.id;
+  const data = req.body.data;
+
+  const user = await User.update({ ...data }, { where: { id } });
+  res.status(200).send("updated");
+};
+
+module.exports = {
+  add_user,
+  get_all_users,
+  get_one_user,
+  delete_user,
+  update_user,
+};
--- index.js
+++ index.js
@@ -13,8 +13,24 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.get("/add", (req, res) => {
-  user_controller.addUser(req, res);
+app.post("/add_user", (req, res) => {
+  user_controller.add_user(req, res);
+});
+
+app.get("/users", (req, res) => {
+  user_controller.get_all_users(req, res);
+});
+
+app.get("/users/:id", (req, res) => {
+  user_controller.get_one_user(req, res);
+});
+
+app.delete("/delete_users/:id", (req, res) => {
+  user_controller.delete_user(req, res);
+});
+
+app.patch("/update_user", (req, res) => {
+  user_controller.update_user(req, res);
 });
 
 app.listen(5001, () => console.log("server running"));
